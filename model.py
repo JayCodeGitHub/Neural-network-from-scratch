3d95ccf109d6ded560ce2aea8c4b5abdea966ac2
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-11 16:45:08 +0200

    Add Model Class with Model Methods

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
+class model():
+    def __init__():
+        print("model")
 
+    def fit():
+        print("fit")
+    def evaluate():
+        print("evaluate")
 
-
-def model():
-    print("model")
+    
+    
