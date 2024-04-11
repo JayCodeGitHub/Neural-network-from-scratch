eca04eaf41d3acb9b347d71bdc1d932b7aae4a31
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-11 09:20:25 +0200

    Fix dataset showcase

--- network.py
+++ network.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from dataset import x_train, x_test
+from dataset import x_train, x_test, y_train
 
 
-print(x_train[0])
 
 
 def network():
     print(x_train[0])
-    plt.imshow(x_test[0])
+    plt.imshow(x_train[0])
     plt.show()
+    print(y_train[0])
 
