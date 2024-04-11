f7d1127471f8f76fb4799784d67ffb4d2af4c1c1
JayCode Developer <JayCode.contakt@gmail.com>
2024-04-11 08:23:59 +0200

    Move dataset to separate file

--- network.py
+++ network.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from dataset import x_train, x_test
 
-mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+print(x_train[0])
 
 
 def network():
-    print(x_train)
+    print(x_train[0])
+    plt.imshow(x_test[0])
+    plt.show()
 
 
